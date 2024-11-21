793e454dc445ece3a5f326560bc942dc1892e123
Michael <michael.pecora.92@gmail.com>
2024-11-21 15:56:49 +0100

    edit

--- frontend/App.js
+++ frontend/App.js
@@ -5,5 +5,7 @@ import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return <AppNavigator />;
+  return (    <View style={{ flex: 1 }}>
+    <AppNavigator />
+  </View>)
 }
